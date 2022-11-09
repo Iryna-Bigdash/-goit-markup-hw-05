b7539f75354e1fc1e30f6019314705e39b38ee98
Iryna B <imbigdash@gmail.com>
2022-11-09 23:15:15 +0000

    added positioning and animation

--- js/modal.js
+++ js/modal.js
@@ -14,8 +14,3 @@
   })();
 
 
-//   const { height: pageHederHeight} = document
-//   .querySelector(".page-header")
-//   .getBoundingClientRect ();
-//   document.body.style.paddingTop = `${pageHederHeight}px`;
-  
